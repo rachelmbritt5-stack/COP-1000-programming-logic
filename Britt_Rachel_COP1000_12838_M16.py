61fffd1f0a6487d883cb6b2e2e0760b8b4537a54
Rachel Britt <rachelmbritt5@gmail.com>
2026-04-14 18:24:55 -0400

    Update baseball quiz questions and answers

--- Britt_Rachel_COP1000_12838_M16.py
+++ Britt_Rachel_COP1000_12838_M16.py
@@ -1,32 +1,40 @@
 hits = 0
 strikes = 0
 userAnswer = ""
-questions = ["What MLB Team Is In Tampa Bay?", "What MLB Team Is In Arlington?", "What MLB Team Is In Baltimore?", "What MLB Team Is In St. Louis?","What MLB Team Is In Cincinnati"]
-answers = ["rays", "rangers", "orioles", "cardinals","reds"]
-numQuestions = len(questions) - 1
+questions = ["What MLB Team Is In Tampa Bay?", "What MLB Team Is In Arlington?", "What MLB Team Is In Baltimore?", "What MLB Team Is In St. Louis?","What MLB Team Is In Detroit"]
+answers = ["rays", "rangers", "orioles", "cardinals","tigers"]
+numQuestions = len(questions) - 1 
 currentQuestion = 0
 
 print("Welcome to the Baseball Quiz!")
 print("Answer the questions.")
 print("3 ‘Strikes’ and you’re out! \n")
 
+# loop through quiz
 while strikes < 3 and currentQuestion <= numQuestions:
-    userAnswer = input(questions[currentQuestion] + "\nAnswer: ").lower()
+    # Prompt user and force lowercase for answer matching
+    userAnswer = input(questions[currentQuestion] + "\nAnswer: ").lower() 
 
+    # block executed if user provides correct answer
     if userAnswer == answers[currentQuestion]:
         print("Hit!\n")
         currentQuestion = currentQuestion + 1
-        hits = hits + 1
+        # increases correct answer count by 1
+        hits = hits + 1 
 
+    # block executed if user provides incorrect answer
     else:
         print("Strike!\n")
         currentQuestion = currentQuestion + 1
+        # increase incorrect answer count by 1
         strikes = strikes + 1
 
+# block executed if user incorrectly answered three questions
 if strikes == 3:
     print("You're out!")
     print("You are " + str(hits) + " for " + str(currentQuestion) + "!")
 
+# block executed if user successfully completed the quiz
 else:
     print("Congrats!")
     print("You are " + str(hits) + " for " + str(currentQuestion) + "!")
